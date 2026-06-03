ee088cd0ab7931324248e2b0267b1a37fcf0ed89
aasthakumarii <aasthakumari.0105@gmail.com>
2026-06-03 15:38:09 +0530

    fixed backend

--- terraform/modules/ec2/userdata.sh
+++ terraform/modules/ec2/userdata.sh
@@ -34,3 +34,9 @@ systemctl enable docker
 systemctl start docker
 
 usermod -aG docker ubuntu
+
+sudo apt update && sudo apt install -y curl unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
